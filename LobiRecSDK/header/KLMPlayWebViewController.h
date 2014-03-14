2f0167cbc34c53ba72f3c5afb0a2ca77c1df2d97
Takahashi Kohei <takahashi-kohei@kayac.com>
2014-03-14 16:04:27 +0900

    playWebViewの初期画面設定処理を追加

--- LobiRecSDK/header/KLMPlayWebViewController.h
+++ LobiRecSDK/header/KLMPlayWebViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, KLMPlayWebDefaultView)
+{
+    KLMPlayWebViewNone    = -1,
+    KLMPlayWebViewProfile = 0,
+    KLMPlayWebViewList,
+    KLMPlayWebViewHelp,
+};
+
 @interface KLMPlayWebViewController : UIViewController
-@property (nonatomic, copy) void(^completionBlock)(void);
+@property (nonatomic, copy)   void(^completionBlock)(void);
+@property (nonatomic, assign) KLMPlayWebDefaultView defaultView;
 @end
